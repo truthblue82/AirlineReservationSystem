04a7fd2983f645b2b423911d106c834f6ce23a39
Yen Truong <truthblue82@gmail.com>
2023-10-05 13:28:24 -0500

    remove server folder, add 3 microservices: user, reservation, management

---
+++ reservation/src/test/java/com/ars/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
