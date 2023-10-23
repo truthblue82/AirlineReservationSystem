993eb6e9e7ab9aee6084137d23797e599388933d
Yen Truong <truthblue82@gmail.com>
2023-10-23 00:47:29 -0500

    add layout for flight-booking

--- client/src/app/pages/flight-booking/flight-booking.component.ts
+++ client/src/app/pages/flight-booking/flight-booking.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-flight-booking',
   templateUrl: './flight-booking.component.html',
   styleUrls: ['./flight-booking.component.scss']
 })
-export class FlightBookingComponent {
+export class FlightBookingComponent implements OnInit {
+  displayModal: boolean = false;
+  constructor(){}
 
+  ngOnInit(): void {
+    //
+  }
 }
