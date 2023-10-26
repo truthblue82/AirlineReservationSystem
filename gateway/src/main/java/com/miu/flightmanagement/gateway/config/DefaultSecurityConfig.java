3c0c43054c4f9e9a003910301b0ff24dff74768b
ldkhoa83@gmail.com <ldkhoa83@gmail.com>
2023-10-25 20:33:02 -0700

    remove redundant code

--- gateway/src/main/java/com/miu/flightmanagement/gateway/config/DefaultSecurityConfig.java
+++ gateway/src/main/java/com/miu/flightmanagement/gateway/config/DefaultSecurityConfig.java
@@ -34,15 +34,12 @@ public class DefaultSecurityConfig {
                 })
                 .csrf(csrfSpec -> csrfSpec.disable())
                 .authorizeExchange(authorize -> authorize
-                        .pathMatchers("/headerrouting/**").permitAll()
                         .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/eureka/**").permitAll()
                         .pathMatchers("/oauth2/**").permitAll()
                         .pathMatchers("/users/registration*").permitAll()
                         .pathMatchers("/login/**").permitAll()
                         .pathMatchers("/error/**").permitAll()
-                        .pathMatchers("/openapi/**").permitAll()
-                        .pathMatchers("/webjar/**").permitAll()
                         .anyExchange().authenticated()).oauth2ResourceServer(oAuth2ResourceServerConfigurer ->
                         oAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults()));
 
